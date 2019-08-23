78eb46d4cacaea5ae2007fb25bfb18e8f6c40868
Anum <anum.amin@tenpearls.com>
2019-08-23 18:20:38 +0500

    WIP

--- News/settings.py
+++ News/settings.py
@@ -50,6 +50,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+  ),
+}
+
 ROOT_URLCONF = 'News.urls'
 
 TEMPLATES = [
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'News.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'auth',
+        'USER': 'django_auth',
+        'PASSWORD': 'test123@',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
